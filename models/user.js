be5ca7bfd795828bd79767868c718dd24d99a7cd
Raj <rajsanghavi9@gmail.com>
2021-02-01 19:36:45 +0530

    Added user model

---
+++ models/user.js
@@ -0,0 +1,38 @@
+// Include libraries
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// profile_pic => String => urlof that
+// Instead of timestamps
+createdAt:{
+    type:String,
+    default:moment().format("DD/MM/YYYY") + moment().format("hh:mm:ss")
+}
+
+const userSchema = new Schema({
+    userName:{
+        type:String,
+        required:true
+    },
+    userEmail:{
+        type:String,
+        required:true
+    },
+    userPassword:{
+        type:String,
+        required:true
+    },
+    profile_pic:{
+        type:String,
+        default:"empty-avatar.jpg"
+    }
+}, {
+        timestamps : true
+    }
+);
+
+// Create User Model
+mongoose.model("userSchema",userSchema);
+
+// export mongoose model
+module.exports = mongoose.model("userSchema") 
