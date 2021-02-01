2999ad9a03d2793769958a8d9eacbd0eb897706d
Raj <rajsanghavi9@gmail.com>
2021-02-01 16:47:28 +0530

    Starter Code

---
+++ index.js
@@ -0,0 +1,29 @@
+// Include libraries
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+
+const app=express();
+const port=process.env.PORT;
+
+// Middlewares
+app.use(cors());
+app.use(morgan());
+
+
+
+// Routes
+app.get('/' , (req,res) => {
+    return res.status(200).json({
+        "status":true,
+        "message":"Amazon Clone Rest API Home Page"
+    })
+
+})
+
+// Start Server
+app.listen(port , () => {
+    console.log(`We are connected at port : ${port}`);
+    
+})
